7ec86b7aeb938203ec5100ece70d68b1d19b4461
AlamCahya <alamcahya3211@gmail.com>
2017-10-25 00:27:42 +0700

    bagian pembeli

---
+++ soal1b.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+        key_t key = 1243;
+	int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
+        int* stock;
+        int menu,i,tstock;
+	char weapon[6][10]={"MP4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
+	char tweapon[10];
+	stock=(int*)shmat(shmid, NULL, 0);
+	
+        printf("KUY BELI SENJATA (pembeli) : ");
+	printf("\n1. Lihat Stock Senjata\n2. Beli Senjata\n3. Keluar\n");
+	while(1){
+	
+	scanf("%d",&menu);
+        switch(menu){
+	case 1:
+	for(i=0;i<=5;i++){
+		if (stock[i]!=0)
+		printf("%s stock %d\n", weapon[i],stock[i]);
+		
+}	
+	break;
+	
+	case 2:
+	scanf("%s %d", tweapon, &tstock);
+	for(i=0;i<=5;i++){
+		if(strcmp(tweapon, weapon[i])==0 && tstock<=stock[i])
+			stock[i]-=tstock;			
+	}
+	break;
+	case 3:
+	return 0;
+	default:
+	printf("MENU GAADA CUY HEHE\n");		
+        
+       		}
+	}
+	shmdt(stock);
+        shmctl(shmid, IPC_RMID, NULL);
+}
