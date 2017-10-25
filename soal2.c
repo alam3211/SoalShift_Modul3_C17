0935ef85d16bcf792abaf72f741664caac9811d3
FalahFilano <falahnurli10@gmail.com>
2017-10-25 21:44:45 +0700

    FIX

--- soal2.c
+++ soal2.c
@@ -6,24 +6,24 @@
  
 pthread_t player1;
 pthread_t player2;
-int nomor[2][16], point[2]={0,0};
-int turn=1;
+int nomor1[16],nomor2[16], point[2]={0,0};
+int mengisi, menebak, turn=1;
  
-void* tulis(void *arg)
+void* play1(void *arg)
 {	
-	turn=2;
-	int i, j, mengisi, menebak;
-    	printf("---player1 mengisi ranjau---\n");
+	
+	int i, j;
+    	printf("\n---player1 mengisi ranjau---\n");
 	for(i=1;i<=4;i++){
 		scanf("%d", &mengisi);		
-		if(mengisi<=16) nomor[1][mengisi]=1;
+		if(mengisi<=16) nomor1[mengisi]=1;
 		else break;	
 	}
 
-	printf("---player2 menebak ranjau---\n");
+	printf("\n---player2 menebak ranjau---\n");
 	for(j=1;j<=4;j++){
 		scanf("%d", &menebak);		
-		if(nomor[1][menebak]==1){
+		if(nomor1[menebak]==1){
 			point[2]+=1;
 			printf("ada ranjau\n");
 			printf("Point player2= %d\n", point[2]);
@@ -37,26 +37,25 @@ void* tulis(void *arg)
 	}
     	
 	printf("\nPoint player1= %d\n", point[1]);
-	printf("Point player2= %d\n", point[2]);	
+	printf("Point player2= %d\n", point[2]);
+	turn=2;	
 }
 
 
-void* baca(void *arg)
+void* play2(void *arg)
 {
-	turn=1;
-	int i, j, mengisi, menebak;	
-	printf("---player2 mengisi ranjau---\n");
+	int i, j;	
+	printf("\n---player2 mengisi ranjau---\n");
 	for(i=1;i<=4;i++){
 		scanf("%d", &mengisi);		
-		if(mengisi>16) break;
-		else nomor[2][mengisi]=1;
-		printf("i %d\n", i);	
+		if(mengisi<=16) nomor2[mengisi]=1;
+		else break;
 	}
 
-	printf("---player1 menebak ranjau---\n");
+	printf("\n---player1 menebak ranjau---\n");
 	for(j=1;j<=4;j++){
 		scanf("%d", &menebak);		
-		if(nomor[2][menebak]==1){
+		if(nomor2[menebak]==1){
 			point[1]+=1;
 			printf("ada ranjau\n");
 			printf("Point player1= %d\n", point[1]);
@@ -71,22 +70,37 @@ void* baca(void *arg)
 
 	printf("\nPoint player1= %d\n", point[1]);
 	printf("Point player2= %d\n", point[2]);
+	turn=1;
 }
  
-int main(void){
+int ceklubang(){
+	int i;
+	for(i=1;i<=16;i++){
+		if(nomor1[i]==0||nomor2[i]==0) return 0;	
+	}
+	return 1;	
+}
 
-/*	for(i=1;i<=16;i++){
-		printf("%d ", nomor[1][1]);
-		printf("%d ", nomor[2][2]);
-		printf("%d\n", i);	
-	}	
-*/	
+int main(){	
+	
 	while(1){
-		if(turn==1) pthread_create(&(player1), NULL, &tulis, NULL);
-    		else if(turn==2) pthread_create(&(player2), NULL, &baca, NULL);
+		//printf("---turn %d---\n", turn);
+		if(turn==1) pthread_create(&(player1), NULL, &play1, NULL);
+    		else if(turn==2) pthread_create(&(player2), NULL, &play2, NULL);
 		pthread_join(player1, NULL);
     		pthread_join(player2, NULL);
+		
+		if(point[1]>=10){
+			printf("\n---player1 menang---\n");
+			break;		
+		}else if(point[2]>=10){
+			printf("\n---player2 menang---\n");
+			break;		
+		}else if(ceklubang()==1){
+			printf("\n---lubang penuh---\n");
+			break;
+		}		
  	}
-   
+   	
     	return 0;
 }
